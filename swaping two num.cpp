cd0358218cbbf75ffb93dd5efa90723b22115adb
himabindubilla123 <122339716+himabindubilla123@users.noreply.github.com>
2023-01-27 19:42:33 +0530

    Add files via upload

---
+++ swaping two num.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 12, b = 13, temp;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
