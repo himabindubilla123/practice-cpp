b298df55efa392b30d336795fad9636ddb796e5e
himabindubilla123 <122339716+himabindubilla123@users.noreply.github.com>
2023-01-27 19:29:52 +0530

    Add files via upload

---
+++ functions2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// declaring a function
+int product(int a, int b) {
+    return (a * b);
+}
+
+int main() {
+
+    int product;
+
+    // calling the function and storing
+    // the returned value in sum
+    product= (2, 3);
+
+    cout << "2 *  " << product << endl;
+
+    return 0;
+}
