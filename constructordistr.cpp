7f2dc27a9bb398e80e294f5fb2592bbb06c88c48
himabindubilla123 <122339716+himabindubilla123@users.noreply.github.com>
2023-01-27 19:00:03 +0530

    Add files via upload

---
+++ constructordistr.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+
+public:
+    Person(){
+        cout << "Constructor is executed" << endl;
+    }
+
+//Destructors
+    // also have same name as class name beginning with ~
+    // you can only have one destructor
+    // cannot overload destructor
+    //like constructor, it doesnt require any return type and parameter
+    ~Person(){
+        cout << "Destructor is executed" << endl;
+    }
+
+};
+
+int main(){
+    Person p1, p2, p3, p4;
+    Person *p5 = new Person;
+
+    delete p5;
+}
