569dabdd1fb07ca5898622b2c7c586f652939bcc
himabindubilla123 <122339716+himabindubilla123@users.noreply.github.com>
2023-01-27 19:24:24 +0530

    Add files via upload

---
+++ c++codeblocks.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string firstName = "hima";
+    string lastName = "bindu";
+    cout<<"concatination :"<<(firstName + lastName)<<endl;
+
+
+}
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    //strings
+//    // string myString = "Hello World";
+//    //+
+//    //indexing starts from 0           [n -1]
+//    string firstName = "Hello";
+//    string space = " ";
+//    string lastName = "World";
+//    // string fullName = firstName + space + lastName;
+//
+//    // string a = firstName.append(lastName);
+//    // int a = firstName.length();
+//    firstName[4] = 'u';
+//    cout << firstName <<endl;
+//
+//
+//    //Take first name from user
+//    //Take last name from user
+//    //Print both names (concatination)
+//
+//}
